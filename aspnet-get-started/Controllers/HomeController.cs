0c222ca7c2e08054bf33cb1865a3a41752085053
lcephas <73600344+lcephas@users.noreply.github.com>
2024-11-29 16:35:39 +0000

    .

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace aspnet_get_started.Controllers
     {
         public ActionResult Index()
         {
-            ViewData["Foo"] = System.Configuration.ConfigurationManager.AppSettings["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+            ViewData["Foo"] = System.Configuration.ConfigurationManager.AppSettings["WEBSITE_NODE_DEFAULT_VERSION"];
             return View();
         }
 
