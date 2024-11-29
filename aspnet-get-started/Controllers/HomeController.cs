dc83dabeb6aed335d1126d8310ce6873e650e3e5
lcephas <73600344+lcephas@users.noreply.github.com>
2024-11-29 15:56:56 +0000

    .

--- aspnet-get-started/Controllers/HomeController.cs
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace aspnet_get_started.Controllers
         public ActionResult Index()
         {
             return View();
-            ViewData["Foo"] = ConfigurationManager.AppSettings["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+            ViewData["Foo"] = System.Configuration.ConfigurationManager.AppSettings["APPLICATIONINSIGHTS_CONNECTION_STRING"];
         }
 
         public ActionResult About()
